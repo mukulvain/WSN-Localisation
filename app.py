54809c033bb5289cebdea3c3e336e72a62190902
Mukul Jain <mukul.jain1906@yahoo.com>
2023-08-31 00:45:11 +0530

    Basic Functions

--- app.py
+++ app.py
@@ -1,13 +1,63 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
 import json
+import datetime
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+
 app = Flask(__name__)
 
-@app.route('/wifi_environments', methods=["GET", 'POST'])
+data = {}
+index = 0
+
+
+@app.route("/wifi_environments", methods=["GET", "POST"])
 def collect_data():
+    global index, data
     if request.method == "GET":
-        return json.dumps({
-            "STATUS": "UP AND RUNNING"
-        })
-    json_data = request.get_json()
-    print(json_data)
-    return json.dumps({"status": "OK", "message": "Successful"})
+        return json.dumps({"STATUS": "UP AND RUNNING"})
+    try:
+        json_data = request.get_json()
+        data[index] = json_data["wifi_networks"]
+        index += 1
+        return json.dumps({"status": "Accepted", "message": "Data Received"})
+    except:
+        return json.dumps({"status": "Failed"})
+
+
+@app.route("/download_data", methods=["GET"])
+def save_data():
+    global data
+    now = int(datetime.datetime.now().timestamp())
+    with open(f"database/data_{now}.json", "w") as f:
+        json.dump(data, f)
+    return send_file(f"database/data_{now}.json", as_attachment=True)
+
+
+@app.route("/clear_data", methods=["GET"])
+def clear_data():
+    global data
+    password = request.form["password"]
+    print(password)
+    print(os.getenv("CLEAR_DATA"))
+    if password == os.getenv("CLEAR_DATA"):
+        data.clear()
+        return json.dumps({"status": "Accepted"})
+    else:
+        return json.dumps({"status": "Failed"})
+
+
+@app.route("/reset_database", methods=["GET"])
+def reset_database():
+    password = request.form["password"]
+    if password == os.getenv("RESET_DATABASE"):
+        current_dir = os.path.join(os.getcwd(), "database")
+        files = os.listdir(current_dir)
+        print(files)
+        for file in files:
+            os.remove(os.path.join(current_dir, file))
+        return json.dumps({"status": "Accepted"})
+    else:
+        return json.dumps({"status": "Failed"})
--- gunicorn.conf.py
+++
@@ -1,4 +0,0 @@
-import os
-
-bind = '0.0.0.0:5000'
-app = os.path.join(os.getcwd(), 'app.py')
